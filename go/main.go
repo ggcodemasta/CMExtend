c985d5c653b13bbba2e7124fc893eb93ba6532e4
Grant <grant_yao@live.ca>
2019-10-28 22:48:27 -0700

    docker python runs

--- go/main.go
+++ go/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func run(command string, args ...string){
@@ -17,15 +20,26 @@ func run(command string, args ...string){
 	}
 }
 
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 func main(){
- asd := `package main;import "fmt" ;func main() {fmt.Print("hello")}`
+
+ 	asd := `package main;import "fmt" ;func main() {fmt.Print("hello")}`
 	err := ioutil.WriteFile("test.go", []byte(asd), 0755)
 	if err != nil {
 		fmt.Printf("Unable to write file: %v", err)
 	}
 	run("go", "run", "test.go")
 	run("pwd")
-	run("python3.6", "../python/main.py")
+	defer timeTrack(time.Now(), "run")
+	run("python3.6", "./python/test.py")
 
+	//http.HandleFunc("/", handler)
+	//log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
